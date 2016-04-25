0b258aaff3f45315d555a781c95c974041d8aa6f
Carlos Gómez <andres1537@gmail.com>
2016-04-24 19:51:44 -0500

    1

--- src/test/java/com/cgomez/AbstractMethod.java
+++ src/test/java/com/cgomez/AbstractMethod.java
@@ -4,6 +4,9 @@
 package com.cgomez;
 
 public abstract class AbstractMethod {
-    protected static String trainFile = "src/test/resources/dblp/jlee_train_f0.txt";
-    protected static String testFile = "src/test/resources/dblp/jlee_test_f0.txt";
+//    protected static String trainFile = "src/test/resources/dblp/jlee_train_f0.txt";
+//    protected static String testFile = "src/test/resources/dblp/jlee_test_f0.txt";
+    
+    protected static String trainFile = "C:/Users/andres/Desktop/data/dl-author-name-desambiguator/resources/dataset/trainingSet.txt";
+    protected static String testFile = "C:/Users/andres/Desktop/data/dl-author-name-desambiguator/resources/dataset/testSet.txt";
 }
