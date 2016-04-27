94bae7413a9a1262c4ce8c9257d76fcd177004f3
Carlos Gómez <andres1537@gmail.com>
2016-04-27 15:40:45 -0500

    1

---
+++ src/test/java/com/cgomez/indi/MainTest.java
@@ -0,0 +1,16 @@
+package com.cgomez.indi;
+
+import com.cgomez.AbstractMethod;
+
+import bdbcomp.Indi;
+
+public class MainTest extends AbstractMethod {
+    public static void main(String[] args) throws Exception {
+        System.out.println("----------------------------------------------");
+        System.out.println("iNDI");
+//        System.out.println("K: 0.0 ± 0.00 :: pF1: 0.0 ± 0.00");
+        System.out.println("----------------------------------------------");
+        String[] arguments = { "collection 9999 9999 0.1 0.9" };
+        Indi.main(arguments);
+    }
+}
