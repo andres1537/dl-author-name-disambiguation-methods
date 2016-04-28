b63b54608c828165ce29192820280cf2f191a385
Carlos Gómez <andres1537@gmail.com>
2016-04-27 21:56:30 -0500

    1

--- src/test/java/com/cgomez/indi/MainTest.java
+++ src/test/java/com/cgomez/indi/MainTest.java
@@ -8,6 +8,7 @@ public class MainTest extends AbstractMethod {
         System.out.println("iNDI");
 //        System.out.println("K: 0.0 ± 0.00 :: pF1: 0.0 ± 0.00");
         System.out.println("----------------------------------------------");
+//        String[] arguments = { "src/test/resources/indi/collection", "1987", "2007", "0.1", "0.9" };
         String[] arguments = { indiFile, "9999", "9999", "0.1", "0.9" };
         Main.main(arguments);
     }
