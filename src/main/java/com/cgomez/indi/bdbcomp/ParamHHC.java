66aa97020e6eee6b866d25286d94fdecfee083b5
Carlos Gómez <andres1537@gmail.com>
2016-04-28 15:44:24 -0500

    1

---
+++ src/main/java/com/cgomez/indi/bdbcomp/ParamHHC.java
@@ -0,0 +1,13 @@
+package com.cgomez.indi.bdbcomp;
+
+class ParamHHC
+{
+  public double title;
+  public double venue;
+  
+  public ParamHHC(double title, double venue)
+  {
+    this.title = title;
+    this.venue = venue;
+  }
+}
