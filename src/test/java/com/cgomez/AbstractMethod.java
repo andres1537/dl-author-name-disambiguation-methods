c6bbd894494e5bfda4a29df17508bd45be227afe
Carlos Gómez <andres1537@gmail.com>
2016-05-19 12:03:28 -0500

    1

--- src/test/java/com/cgomez/AbstractMethod.java
+++ src/test/java/com/cgomez/AbstractMethod.java
@@ -7,7 +7,7 @@ public abstract class AbstractMethod {
 //    protected static String trainFile = "src/test/resources/dblp/jlee_train_f0.txt";
 //    protected static String testFile = "src/test/resources/dblp/jlee_test_f0.txt";
     
-    protected static String trainFile = "C:/Users/andres/Desktop/data/dl-author-name-desambiguator/resources/dataset/txt/no incremental/trainingSet.txt";
-    protected static String testFile = "C:/Users/andres/Desktop/data/dl-author-name-desambiguator/resources/dataset/txt/no incremental/testSet.txt";
-    protected static String indiFile = "C:/Users/andres/Desktop/data/dl-author-name-desambiguator/resources/dataset/txt/incremental/indi";
+    protected static String trainFile = "C:/Users/andres/Desktop/data/dl-author-name-disambiguator/resources/dataset/txt/no incremental/trainingSet.txt";
+    protected static String testFile = "C:/Users/andres/Desktop/data/dl-author-name-disambiguator/resources/dataset/txt/no incremental/testSet.txt";
+    protected static String indiFile = "C:/Users/andres/Desktop/data/dl-author-name-disambiguator/resources/dataset/txt/incremental/indi";
 }
