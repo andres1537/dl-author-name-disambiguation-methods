6d9ba6e4b7aaa6a3b3c1cb9aa51f30466fe27c47
Carlos Gómez <andres1537@gmail.com>
2016-06-03 11:45:10 -0500

    1

--- src/test/java/com/cgomez/indi/MainTest.java
+++ src/test/java/com/cgomez/indi/MainTest.java
@@ -6,7 +6,7 @@ import com.cgomez.indi.bdbcomp.Main;
 public class MainTest extends AbstractMethod {
     public static void main(String[] args) throws Exception {
         System.out.println("----------------------------------------------");
-        System.out.println("iNDI");
+        System.out.println("1. INDi");
 //        System.out.println("K: 0.0 ± 0.00 :: pF1: 0.0 ± 0.00");
         System.out.println("----------------------------------------------");
 //        String[] arguments = { "src/test/resources/indi/collection", "1987", "2007", "0.1", "0.9" };
