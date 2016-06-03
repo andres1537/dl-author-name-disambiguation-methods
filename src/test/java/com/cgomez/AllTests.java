cf60bc58d7e33ee760ca2926a8a7a65e0a2449fe
Carlos Gómez <andres1537@gmail.com>
2016-06-03 11:16:18 -0500

    1

--- src/test/java/com/cgomez/AllTests.java
+++ src/test/java/com/cgomez/AllTests.java
@@ -3,6 +3,7 @@ package com.cgomez;
 public class AllTests extends AbstractMethod {
     public static void main(String[] args) throws Exception {
 	com.cgomez.indi.MainTest.main(args);
+	com.cgomez.nc.classifier.MainSelfTrainingTest.main(args);
 	com.cgomez.nc.classifier.MainTest.main(args);
         com.cgomez.cosine.classifier.MainTest.main(args);
         // com.cgomez.sland.classifier.MainTest.main(args);
