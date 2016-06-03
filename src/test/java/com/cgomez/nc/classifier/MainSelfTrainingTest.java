bcabf1a8422ab82b648864c90794b2c2ecc8cecd
Carlos Gómez <andres1537@gmail.com>
2016-06-03 11:45:22 -0500

    1

--- src/test/java/com/cgomez/nc/classifier/MainSelfTrainingTest.java
+++ src/test/java/com/cgomez/nc/classifier/MainSelfTrainingTest.java
@@ -5,7 +5,7 @@ import com.cgomez.AbstractMethod;
 public class MainSelfTrainingTest extends AbstractMethod {
     public static void main(String[] args) {
         System.out.println("----------------------------------------------");
-        System.out.println("1. NC-Self-Training");
+        System.out.println("2. NC-Self-Training");
         System.out.println("----------------------------------------------");
         String[] arguments = { "-c", testFile, "-e", "0", "-w", "0.5", "0.3", "0.2", "-d", "0.0", "-g", "0.2", "-p", "0.1" };
         Main.main(arguments);
