2f4278cfb62b4c9f424ae2aaa09b91a4150890de
Carlos Gómez <andres1537@gmail.com>
2016-06-03 12:09:15 -0500

    1

--- src/test/java/com/cgomez/nc/classifier/MainSelfTrainingTest.java
+++ src/test/java/com/cgomez/nc/classifier/MainSelfTrainingTest.java
@@ -1,5 +1,9 @@
 package com.cgomez.nc.classifier;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.cgomez.AbstractMethod;
 
 public class MainSelfTrainingTest extends AbstractMethod {
@@ -7,7 +11,26 @@ public class MainSelfTrainingTest extends AbstractMethod {
         System.out.println("----------------------------------------------");
         System.out.println("2. NC-Self-Training");
         System.out.println("----------------------------------------------");
-        String[] arguments = { "-c", testFile, "-e", "0", "-w", "0.5", "0.3", "0.2", "-d", "0.0", "-g", "0.2", "-p", "0.1" };
-        Main.main(arguments);
+        int beginning = 1;
+        int end = 6;
+        
+        Map<Integer, String> map = files();
+        while (beginning <= end) {
+	    if (map.containsKey("Base_" + beginning)) {
+	        Main.main(new String[] { "-c", map.get("Base_" + beginning), "-e", "0", "-w", "0.5", "0.3", "0.2", "-d", "0.0", "-g", "0.2", "-p", "0.1" });	
+	    }
+	    beginning++;
+	}
+    }
+    
+    public static Map<Integer, String> files() {
+	Map<Integer, String> map = new HashMap<Integer, String>();
+	final File folder = new File(increasesFile);
+	for (final File file : folder.listFiles()) {
+	    int i = file.getName().indexOf("_");
+	    map.put(Integer.valueOf(file.getName().substring(i)), file.getAbsolutePath());         
+	}
+	
+	return map;
     }
 }
