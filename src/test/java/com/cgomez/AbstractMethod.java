099a0d5d53c78662d1964ef3bc017576a00ef775
Carlos Gómez <andres1537@gmail.com>
2016-07-22 16:12:55 -0500

    1

--- src/test/java/com/cgomez/AbstractMethod.java
+++ src/test/java/com/cgomez/AbstractMethod.java
@@ -7,8 +7,8 @@ public abstract class AbstractMethod {
 //    protected static String trainFile = "src/test/resources/dblp/jlee_train_f0.txt";
 //    protected static String testFile = "src/test/resources/dblp/jlee_test_f0.txt";
     
-    protected static String trainFile = "C:/Users/andres/Desktop/data/dl-author-name-disambiguator/resources/dataset/txt/no incremental/trainingSet.txt";
-    protected static String testFile = "C:/Users/andres/Desktop/data/dl-author-name-disambiguator/resources/dataset/txt/no incremental/testSet.txt";
-    protected static String indiFile = "C:/Users/andres/Desktop/data/dl-author-name-disambiguator/resources/dataset/txt/incremental/indi";
-    protected static String ncIncrementalFile = "C:/Users/andres/Desktop/data/dl-author-name-disambiguator/resources/dataset/txt/incremental/ncIncremental";
+    protected static String trainFile = "C:/Users/andres/Desktop/data/dl-author-name-disambiguator/resources/DBLP/dataset/txt/no incremental/trainingSet.txt";
+    protected static String testFile = "C:/Users/andres/Desktop/data/dl-author-name-disambiguator/resources/DBLP/dataset/txt/no incremental/testSet.txt";
+    protected static String indiFile = "C:/Users/andres/Desktop/data/dl-author-name-disambiguator/resources/DBLP/dataset/txt/incremental/indi";
+    protected static String ncIncrementalFile = "C:/Users/andres/Desktop/data/dl-author-name-disambiguator/resources/DBLP/dataset/txt/incremental/ncIncremental";
 }
